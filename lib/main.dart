8e799a6c060d0577779ce6a7a46f03c584b546f5
Ashraf Ali <ashrafalicitp@gmail.com>
2023-07-14 02:20:20 +0600

    predict my mood simple app

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyMoodPage extends StatelessWidget {
+class MyMoodPage extends StatefulWidget {
   const MyMoodPage({Key? key}) : super(key: key);
 
   @override
+  State<MyMoodPage> createState() => _MyMoodPageState();
+}
+
+class _MyMoodPageState extends State<MyMoodPage> {
+  var myMood = ['angry', 'happy', 'laughing', 'romantic', 'sad'];
+  var myCurrentMood = 'angry';
+
+  void predictMyMood() {
+    final _random = new Random();
+
+    var element = myMood[_random.nextInt(myMood.length)];
+
+    setState(() {
+      myCurrentMood = element;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Row(children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Image.asset("images/$myCurrentMood.png"),
+            ),
+          ),
+        ]),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black26,
+            ),
+            child: Text('Predict My Mood'),
+            onPressed: () {
+              predictMyMood();
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
